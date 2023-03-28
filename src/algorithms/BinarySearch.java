70fea15f9ce00a0551a5e919228f56c63275d420
diaa ramadan <diaa.ramdan@visionvalley.net>
2023-03-29 00:43:57 +0200

    add iterative search

--- src/algorithms/BinarySearch.java
+++ src/algorithms/BinarySearch.java
@@ -4,21 +4,39 @@ import java.util.List;
 
 public class BinarySearch {
 
-    private final List<Integer> numbers;
+    private List<Integer> items;
 
-    public BinarySearch(List<Integer> numbers) {
-        if (numbers.size() == 0) throw new IllegalArgumentException("List size is ZERO");
-        this.numbers = numbers;
+    public BinarySearch(List<Integer> items) {
+        if (items.size() == 0) throw new IllegalArgumentException("List size is ZERO");
+        this.items = items;
     }
 
 
-    public Integer search(int number) {
-        return search(0, numbers.size() - 1, number);
+    public Integer recursiveSearch(int number) {
+        return search(0, items.size() - 1, number);
+    }
+
+    public Integer iterativeSearch(int number) {
+        int start = 0;
+        int end = items.size() - 1;
+
+        while (start <= end) {
+            int mid = (start + end) / 2;
+            int guess = items.get(mid);
+            if (number == guess) return mid;
+            else if (number < guess) end = mid - 1;
+            else start = mid + 1;
+        }
+        return null;
+    }
+
+    public void setItems(List<Integer> items) {
+        this.items = items;
     }
 
     private Integer search(int start, int end, int number) {
         int mid = (start + end) / 2;
-        int guess = numbers.get(mid);
+        int guess = items.get(mid);
         if (guess == number) return mid;
         if (start >= end) return null;
         if (guess < number) start = mid + 1;
