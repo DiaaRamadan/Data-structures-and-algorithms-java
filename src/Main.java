e7f51c9e93a7f660d66183a7cfcb12bbf67e5967
diaa ramadan <diaa.ramdan@visionvalley.net>
2023-04-05 11:28:55 +0200

    Add quick and selection sort algorithms

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,19 @@
-import Heap.HeapfiyArray;
+import HashTable.HashTable;
+import algorithms.BinarySearch;
+import algorithms.SortingAlgorithm;
+import com.arrays.Array;
+
+import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        int[] numbers = {5, 3, 8, 4, 1, 2};
-        HeapfiyArray.heapfiy(numbers);
-        System.out.println(HeapfiyArray.getKthLargest(numbers, 1));
+
+        var arr = new int[]{4, 8, 7, 2, 0, 4, 5};
+
+        SortingAlgorithm.quicksort(arr);
+
+        System.out.println(Arrays.toString(arr));
+
     }
 
 
